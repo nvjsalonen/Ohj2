742a563a75a96a6a69d1d9dbacb7e8a815aadf83
Nikolas Salonen <nikolas.salonen@tuni.fi>
2022-01-22 14:04:00 +0200

    Lisättiin käsittely 7:lla jaollisille sekä luvuille jotkat ovat jaollisia molemmilla ohjelman kertoimilla

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -6,18 +6,32 @@ int main()
 {
     int pelien_maara = 0;
     int luku = 1;
-    string jaolliset = "zip";
     cout << "How many numbers would you like to have? ";
     cin >> pelien_maara;
     while(luku <= pelien_maara)
         {
-        cout << luku << endl;
-        ++luku;
-        if ((luku % 3) == 0)
+        if ((luku % 3 == 0) && (luku % 7 == 0))
         {
             cout << "zip"<< endl;
+            cout <<"boing"<< endl;
             ++luku;
         }
+        else if (luku % 7 == 0)
+        {
+            cout <<"boing"<< endl;
+            ++luku;
+        }
+        else if(luku % 3 == 0)
+        {
+            cout <<"zip"<< endl;
+            ++ luku;
+        }
+        else
+        {
+            cout << luku << endl;
+            ++luku;
+        }
+
 
     }
     return 0;
