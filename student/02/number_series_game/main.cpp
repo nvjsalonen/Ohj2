bd8429d83c8210bbcba918e311307eb4c866f994
Nikolas Salonen <nikolas.salonen@tuni.fi>
2022-01-22 12:54:42 +0200

    Syötteen luku ja pelkkä numeroiden listaus

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
+using namespace std;
 
 
 int main()
 {
-    std::cout << "How many numbers would you like to have? ";
+    int pelien_maara = 0;
+    cout << "How many numbers would you like to have? ";
+    cin >> pelien_maara;
+    for (int luku = 1; luku <= pelien_maara; ++luku)
+        {
+        cout << luku << endl;
+        }
 
     return 0;
 }
