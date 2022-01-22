2c6e81267d34ba86791e2f7173cec949e9fa758c
Nikolas Salonen <nikolas.salonen@tuni.fi>
2022-01-22 12:13:35 +0200

    Toisen rivin lisaaminen temperatureen.

--- student/02/temperature/main.cpp
+++ student/02/temperature/main.cpp
@@ -8,6 +8,7 @@ int main()
     cout << "Enter a temperature: ";
     cin >> temperature;
     cout << temperature << " degrees Celsius is " << temperature * 1.8 + 32 << " degrees Fahrenheit" << endl;
+    cout << temperature << " degrees Fahrenheit is " <<(temperature - 32) / 1.8 << " degrees Celsius" << endl;
 
-    return temperature;
+    return 0;
 }
