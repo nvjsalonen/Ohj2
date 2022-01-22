7c678a294caaa3301563bd5ffe9c945e1d7aa162
Nikolas Salonen <nikolas.salonen@tuni.fi>
2022-01-22 13:26:25 +0200

    Lisätty 3:lla jaollisten lukujen käsittely. Muutettu while-silmukaksi

--- student/02/number_series_game/main.cpp
+++ student/02/number_series_game/main.cpp
@@ -5,12 +5,20 @@ using namespace std;
 int main()
 {
     int pelien_maara = 0;
+    int luku = 1;
+    string jaolliset = "zip";
     cout << "How many numbers would you like to have? ";
     cin >> pelien_maara;
-    for (int luku = 1; luku <= pelien_maara; ++luku)
+    while(luku <= pelien_maara)
         {
         cout << luku << endl;
+        ++luku;
+        if ((luku % 3) == 0)
+        {
+            cout << "zip"<< endl;
+            ++luku;
         }
 
+    }
     return 0;
 }
