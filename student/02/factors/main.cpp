49f7e7ff084ad4ef186a786f0bd5d0ec14646951
Nikolas Salonen <nikolas.salonen@tuni.fi>
2022-01-23 14:20:53 +0200

    lisätty factors-tehtävän ratkaisu

--- student/02/factors/main.cpp
+++ student/02/factors/main.cpp
@@ -4,7 +4,39 @@ using namespace std;
 
 int main()
 {
+    int annettu_luku = 0;
     cout << "Enter a positive number: ";
+    cin >>annettu_luku;
+    if (annettu_luku <= 0)
+    {
+        cout << "Only positive numbers accepted" << endl;
+        return 0;
+    }
+    int lyhin_matka = annettu_luku;
+    int a = 1, b = 1;
+    for (int i = 1; i < annettu_luku; i++)
+    {
+        int n = annettu_luku/i;
+        if (i*n == annettu_luku)
+        {
+            int valimatka = n-i;
+            if (valimatka < 0)
+            {
+                valimatka = -valimatka;
+            }
+            if (valimatka < lyhin_matka)
+            {
+                lyhin_matka = valimatka;
+                a = i;
+                b = n;
+            }
 
-    return 0;
+            }
+
+        }
+    cout << annettu_luku << " = "<< a << " * "<< b << endl;
 }
+
+
+
+
