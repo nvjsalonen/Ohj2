46da48769e231fbf934fb4e2a071ead4b0f83dfc
Nikolas Salonen <nikolas.salonen@tuni.fi>
2022-02-15 18:42:44 +0200

    Korjattu semantiikkaa satunnaisen kentän funktiossa

--- student/04/hitori/main.cpp
+++ student/04/hitori/main.cpp
@@ -49,6 +49,7 @@ vector<vector<string>> satunnainenKentta()
             break;
         case 6 :
             syote = "2 3 4 1 4 3 4 4 5 3 5 2 1 2 4 4 2 3 2 1 3 1 1 4 3";
+            break;
         case 7 :
             syote = "5 5 2 3 1 1 2 1 4 2 2 1 4 1 5 3 5 1 3 4 1 1 2 5 2";
             break;
