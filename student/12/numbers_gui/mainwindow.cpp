f34df7e727987013f46511c5c21f2178036ec283
Nikolas Salonen <nikolas.salonen@tuni.fi>
2022-04-27 19:44:55 +0300

    Tehty tiiliä liikuttava funktio

--- student/12/numbers_gui/mainwindow.cpp
+++ student/12/numbers_gui/mainwindow.cpp
@@ -3,6 +3,7 @@
 #include <QGraphicsView>
 #include <gameboard.hh>
 #include <string>
+#include <QKeyEvent>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,6 +17,7 @@ MainWindow::MainWindow(QWidget *parent)
 
 
 
+
 }
 
 MainWindow::~MainWindow()
@@ -23,63 +25,80 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-void MainWindow::on_seedLineEdit_returnPressed()
-{
-    seed_ = ui->seedLineEdit->text();
-    lauta_.init_empty();
-    lauta_.fill(seed_.toInt());
-    ruudut = lauta_.returnBoard();
-    piirraRuutu();
-    tulostaLabelit();
-}
 
 void MainWindow::piirraRuutu()
 {
     QBrush  valkoinenBrush(Qt::white);
     QPen    blackpen(Qt::black);
     blackpen.setWidth(2);
-    for(auto y : ruudut)
+    for(auto y : lauta_.returnBoard())
+    {
         for(auto x : y)
         {
-            ruutu_ = pelikentta_->addRect(x->coords_.first * PRINT_WIDTH*10, x->coords_.second * PRINT_WIDTH*10,
+            ruutu_ = pelikentta_->addRect(x->coords_.second * PRINT_WIDTH*10, x->coords_.first * PRINT_WIDTH*10,
                                           PRINT_WIDTH*10, PRINT_WIDTH*10, blackpen, valkoinenBrush);
-            if(x->value_ == 2)
+
+            if(x->value_ != 0)
             {
 
-            QLabel *text_ = new QLabel;
-            text_->setText(QString::number(x->value_));
-            pelikentta_->addWidget(text_);
-            text_->setGeometry(x->coords_.second * PRINT_WIDTH*10, x->coords_.first* PRINT_WIDTH*10,
-                               PRINT_WIDTH*10, PRINT_WIDTH*10);
+                QLabel *text_ = new QLabel(this,Qt::Window | Qt::WindowStaysOnTopHint);
+                text_->setText(QString::number(x->value_));
+                pelikentta_->addWidget(text_);
+                text_->setGeometry(x->coords_.second * PRINT_WIDTH*10, x->coords_.first* PRINT_WIDTH*10,
+                                   PRINT_WIDTH*10, PRINT_WIDTH*10);
+                text_->setAlignment(Qt::AlignCenter);
+
 
 
 
             }
 
-}
-}
-
-
-
-
-
-
-void MainWindow::tulostaLabelit()
-{
-    for(auto y : ruudut)
-    {
-        for(auto x : y )
-        {
-            QLabel *teksti = new QLabel(this);
-            int arvo = x->value_;
-
-
         }
     }
+}
 
-
-
+void MainWindow::keyPressEvent(QKeyEvent *event)
+{
+    if(event->key() == Qt::Key_W)
+    {
+        lauta_.move(UP, 2048);
+    }
+    if(event->key() == Qt::Key_S)
+    {
+        lauta_.move(DOWN, 2048);
+    }
+    if(event->key() == Qt::Key_A)
+    {
+        lauta_.move(LEFT, 2048);
+    }
+    if(event->key() == Qt::Key_D)
+    {
+        lauta_.move(RIGHT, 2048);
+    }
+    lauta_.new_value(false);
+    piirraRuutu();
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void MainWindow::on_seedSpinBox_valueChanged(int arg1)
+{
+
+    lauta_.init_empty();
+    lauta_.fill(arg1);
+    piirraRuutu();
+}
 
--- student/12/numbers_gui/mainwindow.hh
+++ student/12/numbers_gui/mainwindow.hh
@@ -18,22 +18,27 @@ public:
     ~MainWindow();
 
 protected:
-
-
+    void keyPressEvent(QKeyEvent *event);
+        QGraphicsRectItem *ruutu_;
 
 private slots:
-    void on_seedLineEdit_returnPressed();
+    void on_seedSpinBox_valueChanged(int arg1);
 
 private:
     Ui::MainWindow *ui;
     QGraphicsScene *pelikentta_;
-    QGraphicsRectItem *ruutu_;
     QGraphicsTextItem * text_;
     void piirraRuutu();
-    void tulostaLabelit();
     QString seed_;
     GameBoard lauta_;
     std::vector<std::vector<NumberTile*>> ruudut;
+
+
+
+    const Coords LEFT = {0, -1};
+    const Coords UP = {-1, 0};
+    const Coords RIGHT = {0, 1};
+    const Coords DOWN = {1, 0};
 
 };
 #endif // MAINWINDOW_HH
